c84d6cc9903ec0edcd7d4ff5ac6e6d2bce5da1fd
Abhay Mane <89275570+AbhayMane2002@users.noreply.github.com>
2022-09-30 19:47:30 +0530

    Update marvel.js

--- marvel.js
+++ marvel.js
@@ -5,4 +5,10 @@
     "Actor Name": "Robert Downey Jr",
     "SuperPower": "Powered Armor"
     },
+    {
+    "Character Name": "Captain Marvel",
+    "Reel Name": "Carol Danvers",
+    "Actor Name": "Brie Larson",
+    "SuperPower": "enhanced strength, stamina, agility, and durability, as well as flight, and almost total immunity to toxins and poisons."
+    },
     ]
