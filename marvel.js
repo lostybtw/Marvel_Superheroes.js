56d8f21ae36974f75f4ac5783806c42d1e35316c
Diptiii <110099889+dipti-226@users.noreply.github.com>
2022-09-30 19:47:05 +0530

    new name

--- marvel.js
+++ marvel.js
@@ -5,4 +5,10 @@
     "Actor Name": "Robert Downey Jr",
     "SuperPower": "Powered Armor"
     },
+    {
+    "Character Name": "bruce banner",
+    "Reel Name": "edward norton",
+    "Actor Name": "mark ruffalo ",
+    "SuperPower": "durability"
+    },
     ]
