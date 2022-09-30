2f6fc0adb92409877fbd5b487e2c0be26a0261b2
Prathmesh Tangade <102044916+prathamesh10346@users.noreply.github.com>
2022-09-30 19:42:42 +0530

    blackwido

--- marvel.js
+++ marvel.js
@@ -5,4 +5,19 @@
     "Actor Name": "Robert Downey Jr",
     "SuperPower": "Powered Armor"
     },
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     {
+    "Character Name": "Blackwido",
+    "Reel Name": "Scarleget",
+    "Actor Name": "scarlet jonson",
+    "SuperPower": "Magic power"
+    },
     ]
